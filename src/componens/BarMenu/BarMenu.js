a054b47b5cf79d6308ef80f6585d390cdc65b5ef
Julia-Troyan <147001746+Julia-Troyan@users.noreply.github.com>
2025-01-31 22:51:46 +0300

    Создан компонент Header

---
+++ src/componens/BarMenu/BarMenu.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./style_bar.scss";
+
+const BarMenu = ({ closeMenu }) => {
+  return (
+    <div className="menu-active">
+        <h1 className="menu-active__h1">MENU</h1>
+        <button className="menu-close" onClick={closeMenu}>X</button>
+         <div className="headline"><h2>MAN</h2>
+          <p>Accessories</p>
+          <p>Bags</p>
+          <p>Denim</p>
+          <p>T-Shirts</p>
+        </div>
+        <div className="headline"><h2>WOMAN</h2>
+          <p>Accessories</p>
+          <p>Jackets & Coats</p>
+          <p>Polos</p>
+          <p>T-Shirts</p>
+          <p>Shirts</p>
+        </div>
+        <div className="headline"><h2>KIDS</h2>
+          <p>Accessories</p>
+          <p>Jackets & Coats</p>
+          <p>Polos</p>
+          <p>T-Shirts</p>
+          <p>Shirts</p>
+          <p>Bags</p>
+        </div>
+    </div>
+         
+
+  );
+};
+export default BarMenu;
---
+++ src/componens/Header/Header.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./style_header.scss"
+import Logo from "../Image/Logo";
+import SearchIcon from "../Image/SearchIcon";
+import MenuIcon from "../Image/MenuIcon";
+import UserIcon from "../Image/UserIcon";
+import TruckIcon from "../Image/TruckIcon";
+import BarMenu from "../BarMenu/BarMenu";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const openMenu = () => {
+    setIsMenuOpen(true);
+  };
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+return (
+  <header className="header container1">
+    <div className="topNav container2">
+    <div className="topNav-left">
+      <a href="#"><Logo /></a>
+      <button className="btn_a"><SearchIcon /></button>
+        </div>
+        <div className="topNav-right">
+          <a href="#"><button onClick={openMenu} className="btn_a1 burger"><MenuIcon /></button></a>
+          <a href="#"><button className="btn_a1"><UserIcon /></button></a>
+          <a href="#"><button className="btn_a1"><TruckIcon />
+            <span className="header__count">5</span></button></a>
+        </div>
+        
+     </div>
+     {/* Модальное окно */}
+     {isMenuOpen && (
+        <div className="menu-modal">
+          <BarMenu closeMenu={closeMenu} />
+          
+        </div>
+      )}
+  </header>
+);
+
+}
+export default Header;
